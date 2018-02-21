8b9aa7eaa5ef50237c179bfa3e7e1c4c39423abd
LordHelpUs <mgelban@gmail.com>
2018-02-20 20:32:55 -0800

    update mongodb uri string env vars

--- start.js
+++ start.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 // load env files
 require('dotenv').config( { path: 'variables.env'});
 // DB CONNECTION
-mongoose.connect(process.env.DATABASE); // connect to DB
-// mongoose.Promise = global.Promise; //USE ES6 PROMISES
-// // handle dbconnection errors
-// mongoose.connection.on('error', (err) => {
-//   console.error(`err message ${err.message}`);
-// })
+mongoose.connect(process.env.MONGO_URI, {user: process.env.DB_USER, pass: process.env.DB_PASS}); // connect to DB
+mongoose.Promise = global.Promise; //USE ES6 PROMISES
+// handle dbconnection errors
+mongoose.connection.on('error', (err) => {
+  console.error(`err message ${err.message}`);
+})
 
 // import models
 // require('./models/Store');
