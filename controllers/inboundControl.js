d27c3be7730b2edbe02ce2e48c9d9d464f612961
elgelbo <mgelban@gmail.com>
2018-09-10 21:47:17 -0700

    fixer

--- controllers/inboundControl.js
+++ controllers/inboundControl.js
@@ -1,11 +1,5 @@
 const mongoose = require('mongoose');
 
-// exports.inbound = (req, res, next) => {
-//   next();
-// }
-
-exports.inbound = (req, res) => {
-  console.log('mess');
-  
-  // res.status(200).end();
+exports.inbound = (req, res, next) => {
+  next();
 }
